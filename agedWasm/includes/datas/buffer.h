3e758927dd063a4a6da523b9baf566c9dd70b9e5
huang825172 <1048035187@qq.com>
2019-06-28 03:31:15 +0800

    Adding type decoder and added ASSERT

--- agedWasm/includes/datas/buffer.h
+++ agedWasm/includes/datas/buffer.h
@@ -13,9 +13,7 @@ typedef struct BUFFER_s
 /* Initialize `BUF` with `SIZE` in size and `FILL` to fill with*/
 #define BUFFER_INIT(BUF,SIZE,FILL) do {\
     (BUF).buffer = malloc_a((SIZE)*sizeof(byte));\
-    if(!(BUF).buffer){\
-        panic_a("malloc failed!\n");\
-    }\
+    ASSERT_A((!(BUF).buffer),"malloc failed!\n");\
     (BUF).size = (SIZE);\
     memset_a(BUFFER_DATA((BUF)),(FILL),BUFFER_SIZE((BUF)));\
 } while(0)
@@ -28,26 +26,20 @@ typedef struct BUFFER_s
 
 /* Fill data in size of `SIZ` from `PTR` to buffer `BUF` with offset of buffer`OFFS`*/
 #define BUFFER_FILL(BUF,OFFS,PTR,SIZ) do {\
-    if((OFFS)+(SIZ)>BUFFER_SIZE(BUF) || (SIZ)<0){\
-        panic_a("fill failed!\n");\
-    }\
+    ASSERT_A(((OFFS)+(SIZ)>BUFFER_SIZE(BUF) || (SIZ)<0),"fill failed!\n");\
     i32 i;\
     for(i=0;i<(i32)(SIZ);i++) BUFFER_DATA(BUF)[(OFFS)+i] = (PTR)[i];\
 } while(0)
 
 /* Fill data in size of `SIZ` from buffer `ORI` with offset `OFF` to buffer `BUF` with offset `OFFS` */
 #define BUFFER_SLCE(BUF,OFFS,ORI,OFF,SIZ) do {\
-    if((OFF)+(SIZ)>BUFFER_SIZE(ORI) || (OFFS)+(SIZ)>BUFFER_SIZE(BUF)){\
-        panic_a("slice failed\n");\
-    }\
+    ASSERT_A(((OFF)+(SIZ)>BUFFER_SIZE(ORI) || (OFFS)+(SIZ)>BUFFER_SIZE(BUF)),"slice failed!\n");\
     BUFFER_FILL(BUF,OFFS,(byte*)(BUFFER_DATA(ORI)+OFF),SIZ);\
 } while(0)
 
 /* Read data in size of `SIZ` from buffer `BUF` with offset `OFFS` to `PTR` */
 #define BUFFER_READ(BUF,OFFS,PTR,SIZ) do {\
-    if((OFFS)+(SIZ)>BUFFER_SIZE(BUF) || (SIZ)<0){\
-        panic_a("read failed!\n");\
-    }\
+    ASSERT_A(((OFFS)+(SIZ)>BUFFER_SIZE(BUF) || (SIZ)<0),"read failed!\n");\
     i32 i=0;\
     for(i=0;i<(SIZ);i++) ((byte*)PTR)[i]=BUFFER_DATA(BUF)[(OFFS)+i];\
 } while(0)
---
+++ agedWasm/includes/decoders/typesDecoder/typesDecoder.h
@@ -0,0 +1,9 @@
+#ifndef _TYPESDECODER_H
+#define _TYPESDECODER_H
+
+#include <objects/types/typesObject.h>
+#include <datas/buffer.h>
+
+typesObject_t* decodeTypes(BUFFER* buf);
+
+#endif

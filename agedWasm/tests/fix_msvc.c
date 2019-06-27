093bae2a7322c8f91bc81d647e9627f298b33496
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-06-28 04:01:00 +0800

    make msvc to compile

---
+++ agedWasm/tests/fix_msvc.c
@@ -0,0 +1,15 @@
+//
+// Created by 31838 on 6/28/2019.
+//
+
+#include "unity/unity.h"
+
+#define DO_NOTHING do{}while(0)
+
+void suiteSetUp(void) { DO_NOTHING; }
+
+int suiteTearDown(int num_failures) { DO_NOTHING; }
+
+void setUp(void) { DO_NOTHING; }
+
+void tearDown(void) { DO_NOTHING; }

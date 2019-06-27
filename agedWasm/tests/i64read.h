ac97704688f516fac78ff32b65602f9a5295231b
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-06-28 04:41:38 +0800

    add test

---
+++ agedWasm/tests/i64read.h
@@ -0,0 +1,13 @@
+#include <datas/types.h>
+#include <datas/buffer.h>
+#include <datas/readers/uintReaders/uintReaders.h>
+#include "unity/unity.h"
+
+void test_i64_read() {
+    byte data[3] = {0xE9, 0xBB, 0x07};
+    BUFFER b;
+    BUFFER_INIT(b, 3, 0);
+    BUFFER_FILL(b, 0, data, 3);
+    u64 res = readUint64(&b, 0);
+    TEST_ASSERT_EQUAL_UINT64((u64) 122345, res);
+}
--- agedWasm/tests/tests.c
+++ agedWasm/tests/tests.c
@@ -7,12 +7,14 @@
 #include "readers.h"
 #include "decoder.h"
 #include "vector.h"
+#include "i64read.h"
 
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_buffer);
     RUN_TEST(test_decoder);
-    RUN_TEST(test_readers);
+//    RUN_TEST(test_readers);
     RUN_TEST(test_vector);
+    RUN_TEST(test_i64_read);
     return UNITY_END();
 }

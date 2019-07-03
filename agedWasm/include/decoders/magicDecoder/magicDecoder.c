2ce0358f8a5bafd78a082fc758e1d7939715b5dd
huang825172 <1048035187@qq.com>
2019-07-03 16:40:05 +0800

    porting

--- agedWasm/include/decoders/magicDecoder/magicDecoder.c
+++ agedWasm/include/decoders/magicDecoder/magicDecoder.c
@@ -7,12 +7,20 @@
 #include <stdlib.h>
 
 magicObject_t* decodeMagic(array raw){
-    buffer buf = buffer_init(sizeof(byte));
-    ASSERT_A(array_size(raw)==sizeof_magic+sizeof(i32),"magic decode failed!\n");
+    buffer buf = buffer_from_array(raw);
+    ASSERT_A(buffer_size(buf)==sizeof_magic+sizeof(i32),"magic decode failed!\n");
     i32 i=0;
-    for(i=0;i<sizeof_magic;i++){
-        ASSERT_A((((byte*)array_get_data(raw))[i]==magicCode[i]),"magic check failed!\n");
+    for(i=0;i<sizeof_magic;i++){   
+        array r = array_init(1,sizeof(byte));
+        buffer_read(&r,buf,1);
+        debug_out("%d\n",array_size(r));
+        ASSERT_A( (( (byte*)array_get_data(r))[0]==magicCode[i]),
+        "magic check failed!\n");
+        array_destroy(r);
     }
+    
+        debug_out("hello\n");
+
     BUFFER buff;
     BUFFER_INIT(buff,array_size(raw),0x00);
     BUFFER_FILL(buff,0,(byte*)array_get_data(raw),array_size(raw));
